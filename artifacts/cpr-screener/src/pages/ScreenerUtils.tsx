1fbbb7440938317fc4f861df003acbceafcd367c
krivengokul <krivengokul@gmail.com>
2026-06-29 20:22:47 +0530

    inside-cpr-expanded - (r.todayCPR.r4 > r.prevCPR.r4 || r.todayCPR.s4 < r.prevCPR.s4)

--- artifacts/cpr-screener/src/pages/ScreenerUtils.tsx
+++ artifacts/cpr-screener/src/pages/ScreenerUtils.tsx
@@ -109,7 +109,8 @@ export function passesPattern(r: CPRResult, pattern: string): boolean {
     case "inside-cpr":
       return r.todayCPR.tc < r.prevCPR.tc && r.todayCPR.bc > r.prevCPR.bc;
     case "inside-cpr-expanded":
-      return r.todayCPR.tc < r.prevCPR.tc && r.todayCPR.bc > r.prevCPR.bc && r.todayCPR.r4 > r.prevCPR.r4 && r.todayCPR.s4 < r.prevCPR.s4;
+      return r.todayCPR.tc < r.prevCPR.tc && r.todayCPR.bc > r.prevCPR.bc && 
+              (r.todayCPR.r4 > r.prevCPR.r4 || r.todayCPR.s4 < r.prevCPR.s4);
     case "outside-cpr":
       return r.todayCPR.tc > r.prevCPR.tc && r.todayCPR.bc < r.prevCPR.bc;
     case "outside-cpr-compressed":
