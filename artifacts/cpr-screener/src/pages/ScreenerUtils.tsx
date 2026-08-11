0398ae5a96196daca78b38d9c394a6bb2f511f38
krivengokul <krivengokul@gmail.com>
2026-08-11 07:31:44 +0530

    6PM:APHS1A-FAU4:9PM added extra filters

--- artifacts/cpr-screener/src/pages/ScreenerUtils.tsx
+++ artifacts/cpr-screener/src/pages/ScreenerUtils.tsx
@@ -777,7 +777,9 @@ export function passesPattern(r: CPRResult, pattern: string): boolean {
         r.eXL4U2 &&
         computePrevPattern(r.prevCPR, r.ppCPR) === "eXL4U3" &&
         r.todayCPR.bc > r.prevCPR.prevHigh &&
-        r.todayCPR.s1 > r.prevCPR.tc
+        r.todayCPR.s1 > r.prevCPR.tc &&
+        r.prevCPR.pivot > r.todayCPR.prevLow &&
+        r.todayCPR.s3 > r.prevCPR.s3 
       );
     case "pcpr-u1-cpr-pl1":
       return r.pCPR1Above;
